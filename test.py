fd53ab3192b57bf326abcd22724217b9545535a7
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2024-12-08 14:13:38 -0500

    colors

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
 # Create the window and what it's size will be (by pixels)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+
 circles = []
 rects = []
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('First Game')
+
+BG_COLOR = pygame.Color('grey12')
+hue = 0
+def get_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
 
 
 # --- Main Game Loop ---
@@ -23,19 +34,23 @@ while run:
             run = False
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             x, y = event.pos
-            circles.append((x, y))
+            random_color = get_random_color()
+            circles.append({'position': (x, y), 'random_color': random_color})
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
             x, y = event.pos
-            rects.append((x, y, 20, 20))
+            box_color = pygame.Color(0)
+            box_color.hsva = (hue, 100, 100, 100)
+            hue = (hue + 30) % 360
+            rects.append({'position': (x, y, 20, 20), 'color': box_color})
 
-    screen.fill((0, 0, 0))
+    screen.fill(BG_COLOR)
     for circle in circles:
-        # draw a red circle with a radius of 10 at whatever coordinate you clicked
-        pygame.draw.circle(screen, (255, 0, 0), circle, 10)
+        pygame.draw.circle(screen, circle["random_color"], circle['position'], 10)
     for rect in rects:
-        pygame.draw.rect(screen, (0, 0, 255), rect, 1)
+        pygame.draw.rect(screen, rect['color'], rect['position'], 2)
 
     pygame.display.flip()
+
 
 
 pygame.quit()
